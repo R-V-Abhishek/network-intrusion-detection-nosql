4c72f61ad5bf5e51e113ac15183961c168435e46
R-V-Abhishek <rvabhi2504@gmail.com>
2026-04-22 22:59:25 +0530

    refactor: reorder import statements for clarity in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # Entry point for the Network Intrusion Detection System
 import sys
 import os
+from dashboard.app import app
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from dashboard.app import app
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
